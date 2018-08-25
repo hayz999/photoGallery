699fce867769e537e729d9db01c5687c8988ec85
hayz999 <hlzulkoski@gmail.com>
2018-08-25 13:49:14 -0600

    data loaded and worked on styling

--- anniversary/src/App.js
+++ anniversary/src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
+import Header from './components/Header';
+import Photos from './components/Photos';
 import './App.css';
 
+const url = 'https://photos-server.herokuapp.com/photos';
+
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      photos: []
+    }
+  }
+
+  componentDidMount() {
+    fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      this.setState({photos: data.photos})
+    })
+  }
   render() {
     return (
       <div className="App">
-        
+        <Header />
+        <Photos />
       </div>
     );
   }
